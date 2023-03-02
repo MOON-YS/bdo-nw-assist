5c7dc5b25e775c55dc978b8cb1a63412564e1501
choryang <mys990422@gmail.com>
2023-03-02 21:33:34 +0900

    v0.4

--- discordbot.py
+++ discordbot.py
@@ -17,8 +17,6 @@ import time
 PREFIX = os.environ['PREFIX']
 TOKEN = os.environ['TOKEN']
 
-
-
 def getNwInfoStr(data):
     return "거점명: " + data['area'] + "\n최대인원: " + data['num'] + "\n단계: " + data['stage'] + "\n영지: "+ data['ter']
 
@@ -34,7 +32,7 @@ wd = {0:'월요일', 1:'화요일', 2:'수요일',3:'목요일',4:'금요일',5:
 @bot.event
 async def on_ready():
     print("Bot is ready")
-    await bot.change_presence(status=discord.Status.online, activity=discord.Game("AV"))
+    await bot.change_presence(status=discord.Status.online, activity=discord.Game("NIKKE"))
 
 channel = 0
 full_num = 0
@@ -42,6 +40,9 @@ crnt_num = 0
 np_tdnw = 0
 cur_wd = -1
 pre_wd = -1
+role_attend = 0
+is_roleChecked = False
+is_init = False
 
 """
 def roleCheck(ctx):
@@ -53,25 +54,64 @@ def roleCheck(ctx):
 """
 crnt_usr = pd.DataFrame(columns=['name','guild'])
 crnt_usr.head(10)
+@bot.command()
 
+async def init(ctx):
+    if not ctx.author.top_role.permissions.administrator:
+        await ctx.channel.send(str(ctx.author.mention + "권한이 없습니다."))
+        return
+    
+    global wd ,today_nw, today_nws, full_num, np_tdnw, crnt_num, crnt_usr, is_roleChecked, role_attend, is_init
+    if is_init:
+       await ctx.channel.send(str(ctx.author.mention + "이미 초기화를 했습니다."))
+       return 
+   
+    if not is_roleChecked:
+        for i in range(len(ctx.guild.roles)):
+            if(ctx.guild.roles[i].name == "참여자"):
+                role_attend = ctx.guild.roles[i]
+                is_roleChecked = True
+            else:
+                await ctx.channel.send(str(ctx.author.mention + "참여자 역할이 서버에 존재하지 않습니다."))
+                return
+    
+    is_init = True
+    
+            
 #send today nord war list (1stage)
 @bot.command()
 async def setTd(ctx):
+    global wd ,today_nw, today_nws, full_num, np_tdnw, crnt_num, crnt_usr, is_roleChecked, role_attend, is_init
+    
+    if not is_init:
+        await ctx.channel.send(str(ctx.author.mention + "!init으로 초기화 해주세요"))
+        return
+    
+    if not is_roleChecked:
+        await ctx.channel.send(str(ctx.author.mention + "참여자 역할이 설정되지 않았습니다."))
+        return
+    
     if not ctx.author.top_role.permissions.administrator:
         await ctx.channel.send(str(ctx.author.mention + "권한이 없습니다."))
-        return;
+        return
     
     if datetime.now(timezone('Asia/Seoul')).weekday() == 5:
         await ctx.channel.send("오늘은 거점전이 진행되지 않습니다.")
-        return;
+        return
     
-    global wd ,today_nw, today_nws, full_num, np_tdnw, crnt_num, crnt_usr
     crnt_usr = pd.DataFrame(columns=['name','guild'])
     full_num = 0
     np_tdnw = 0
     crnt_num = 0
     today_nw = 0
     today_nws = nw_data[nw_data['date']==wd[datetime.now(timezone('Asia/Seoul')).weekday()]].astype(str)
+    
+    attends = role_attend.members
+    
+    for usr in attends:
+        await usr.remove_roles(role_attend)
+        
+    
     print(f"updated Today: {wd[datetime.now(timezone('Asia/Seoul')).weekday()]} ")
     s = [""]
     for i in range(0, today_nws['area'].count()):
@@ -82,12 +122,16 @@ async def setTd(ctx):
 
 @bot.command()
 async def setNw(ctx, arg=None):
+    global today_nw, full_num, np_tdnw, is_init
+    
+    if not is_init:
+        await ctx.channel.send(str(ctx.author.mention + "!init으로 초기화 해주세요"))
+        return
     
     if not ctx.author.top_role.permissions.administrator:
         await ctx.channel.send(str(ctx.author.mention + " 권한이 없습니다."))
         return;
     
-    global today_nw, full_num, np_tdnw
     if datetime.now(timezone('Asia/Seoul')).weekday() == 5:
         await ctx.channel.send("오늘은 거점전이 진행되지 않습니다.")
         return;
@@ -124,11 +168,13 @@ async def setNw(ctx, arg=None):
     embed = discord.Embed(title = '금일 거점 지역', description =d)
     await ctx.channel.send(embed=embed)
     
-    
 @bot.command()
 async def 신청(ctx):
     
-    global crnt_num, crnt_usr, full_num
+    global crnt_num, crnt_usr, full_num, is_init, role_attend
+    if not is_init:
+        await ctx.channel.send(str(ctx.author.mention + "!init으로 초기화 해주세요"))
+        return
     
     val1 = '[' not in ctx.author.display_name
     val2 = ']' not in ctx.author.display_name
@@ -161,12 +207,23 @@ async def 신청(ctx):
     print(usr_name + " 이(가) 참여했습니다.")
     crnt_usr.loc[crnt_num] = [usr_name, usr_gld]
     crnt_num = crnt_num+1
+    
+    await ctx.author.add_roles(role_attend)
     await ctx.channel.send(str(ctx.author.mention + f" 감사! {crnt_num}/{full_num}"))
 
 @bot.command()
 async def 취소(ctx):
     
-    global crnt_num, crnt_usr, full_num
+    global crnt_num, crnt_usr, full_num, is_init
+
+    
+    if not is_init:
+        await ctx.channel.send(str(ctx.author.mention + "!init으로 초기화 해주세요"))
+        return
+    
+    if (full_num == 0):
+        await ctx.channel.send(str(ctx.author.mention + " 금일 거점이 설정되지 않았습니다."))
+        return
     
     if crnt_num == 0:
         await ctx.channel.send("err : 리스트가 비어있습니다")
@@ -184,12 +241,17 @@ async def 취소(ctx):
     crnt_usr.drop(usr_n, axis=0, inplace=True)
     crnt_usr.reset_index(inplace=True, drop=True)
     crnt_num = crnt_num-1
+    await ctx.author.remove_roles(role_attend)
     await ctx.channel.send(str(ctx.author.mention + f" 잘가시지~ {crnt_num}/{full_num}"))
 
 @bot.command()
 async def 참가자(ctx):
 
-    global crnt_usr,crnt_num,full_num
+    global crnt_usr,crnt_num,full_num, is_init
+    
+    if not is_init:
+        await ctx.channel.send(str(ctx.author.mention + "!init으로 초기화 해주세요"))
+        return
     output = crnt_usr.to_numpy()
     output = np.sort(output[:,0])
     
@@ -203,8 +265,10 @@ async def 참가자(ctx):
 @bot.command()
 async def 정보(ctx):
 
-    global today_nw, full_num
-    
+    global today_nw, full_num, is_init
+    if not is_init:
+        await ctx.channel.send(str(ctx.author.mention + "!init으로 초기화 해주세요"))
+        return
     if full_num == 0:
         await ctx.channel.send("금일 거점이 설정되지 않았습니다")
         return
@@ -217,6 +281,10 @@ async def 정보(ctx):
 
 @bot.command()
 async def 명령어(ctx):
+    if not is_init:
+        await ctx.channel.send(str(ctx.author.mention + "!init으로 초기화 해주세요"))
+        return
+    
     s = [""]
     s.append("///////관리자용///////")
     s.append("!setTd : 오늘자 거점전 초기화")
@@ -237,6 +305,10 @@ async def sayHere(ctx):
     if not ctx.author.top_role.permissions.administrator:
         await ctx.channel.send(str(ctx.author.mention + " 권한이 없습니다."))
         return
+    
+    if not is_init:
+        await ctx.channel.send(str(ctx.author.mention + "!init으로 초기화 해주세요"))
+        return
 
     channel = ctx.channel
     
@@ -255,6 +327,7 @@ async def sayTest(ctx):
 @tasks.loop(seconds=1)
 async def every_day():
     global cur_wd, pre_wd, channel, wd ,today_nw, today_nws, full_num, np_tdnw, crnt_num, crnt_usr
+    
     pre_wd = cur_wd
     cur_wd = datetime.now(timezone('Asia/Seoul')).weekday()
     
